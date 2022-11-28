e2e54b3f987b4aa2b4a135d6db061954b28ed029
wyhong3103 <wyhong3103@gmail.com>
2022-11-28 20:50:22 +0800

    Remove unnecessary code

--- src/components/CV.js
+++ src/components/CV.js
@@ -1,10 +1,6 @@
 import { Component } from "react";
 
 class CV extends Component{
-    constructor(props){
-        super(props);
-    }
-
     render(){
         const {
             fname, lname,email, phone, desc
